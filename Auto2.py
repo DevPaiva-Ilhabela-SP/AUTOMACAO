b8f1f6c02f42d769cdb06a5023ba43d31a242a49
Rodrigo de Paiva Costa <rodrigo_paiva_costa@hotmail.com>
2024-07-14 23:03:51 -0300

    Atualizações

--- Auto2.py
+++ Auto2.py
@@ -1,5 +1,9 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import time
+
 browser = Firefox()
 
 link = "https://google.com"
@@ -9,19 +13,24 @@ browser.get(link)
 input_area = browser.find_element(By.NAME,"q")
 
 input_area.send_keys("Instituto Joga Junto")
-input_area.send_keys(keys.Enter)
-from time import sleep; sleep(3)
-link_ijj = browser.find_element(By.TAG_NAME,'h3')
-print("result_search")
+input_area.send_keys(Keys.ENTER)
 
+
+time.sleep(5)
+
+result_seach = browser.find_elements(By.TAG_NAME,'h3')
 
 check = False
 
-while check == False:
-
-
-    for result in result_search:
-        if result.text == "Instituto Joga Junto":
+for result in result_seach:
+        if "Instituto Joga Junto" in result.text:
             result.click()
-            print("AEEEEEEEEEE")
+            print("Link selecionado")
         check = True
+        break
+title = browser.title
+assert "Instituto Joga Junto" in title
+
+browser.get("")
+input_area.send_keys("Contato")
+input_area.send_keys(Keys.ENTER)
